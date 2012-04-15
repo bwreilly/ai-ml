0d5f3d20352fe4474b642931238dbed61d621985
Ben Reilly <gallipoli@gmail.com>
2012-04-14 22:00:12 -0700

    addition of particle filter in 2D space

---
+++ pfilter.py
@@ -0,0 +1,147 @@
+import math
+import random
+
+
+def generate_particles(point, k, mdist=None):
+    """ Generate k random particles around the point (x, y) with an optional max distance (mdist).
+    """
+    sd = mdist or 1
+    particles = [point] * k
+
+    def fuzz(x):
+        return x + random.uniform(-1 * sd, sd)
+
+    particles = [((fuzz(p[0]), fuzz(p[1])), 0) for p in particles]
+
+    return particles
+
+
+def normalize(particles):
+    """ Transforms weighted particles into a bayesian distribution
+    >>> normalize([(1, 2), 50, (3, 4), 20, (3, 3), 30])
+    >   [(1, 2), 0.19354838709677416,
+         (3, 3), 0.3225806451612903,
+         (3, 4), 0.4838709677419355]
+    If there is a weight of 0, this function will short-circuit and return [particle] * N
+     """
+    weights = []
+    for p in particles:
+        k = p[0]
+        v = p[1]
+        weights.append((k, (v) * 0.1))
+
+    total = sum([x[1] for x in weights])  # total amount
+    return [(n[0], n[1] / total) for n in weights]  # normalization
+
+
+def sample(particles):
+    """ Given some particles with probability values, pick new ones with
+    replacement based on weights.
+
+    particles = [((x, y), weight), ((x, y), weight)...]
+    """
+    p = []
+    w = [(x[1]) for x in particles]
+    N = len(particles)
+    index = int(random.random() * N)
+    beta = 0.0
+    mw = max(w)
+    for i in range(N):
+        beta += random.random() * 2.0 * mw
+        while beta > w[index]:
+            beta -= w[index]
+            index = (index + 1) % N
+        p.append(particles[index])
+    return p
+
+
+def distance(p1, p2):
+    return math.sqrt(((p1[0] - p2[0]) ** 2) + ((p1[1] - p2[1]) ** 2))
+
+
+def particle_filter(init, movement, evaluate, N, landmarks):
+    """ Given some movement iterable, a number of particles N, a sense function defined as
+
+        def sense(state):
+            return measurements(state)  # iterable
+
+        and an evaluate function that returns an inverse non-normalized weight,
+        given two states.
+
+        This function will return the estimated state at the end of the movements.
+    """
+    position = init
+    particles = generate_particles(position, N)
+    for m in movement:
+
+        # sense step
+        particles = [(p[0], evaluate(p[0], position, landmarks)) for p in particles]
+        particles = normalize(particles)
+        particles = sample(particles)
+
+        # move step
+        particles = [(move(p[0], m), p[1]) for p in particles]
+        position = move(position, m)
+
+    def best(points):
+        x = sum([p[0] for p in points])
+        y = sum([p[1] for p in points])
+        return x / len(points), y / len(points)
+
+    return best([p[0] for p in particles]), position, particles
+
+
+def gaussian(mu, sig, x):
+    """ Probability of x given the normal distribution of mu and sig
+    """
+    try:
+        g = math.exp(- ((mu - x) ** 2) / (sig ** 2) / 2.0) / math.sqrt(2.0 * math.pi * (sig ** 2))
+    except ZeroDivisionError:
+        return 1.0
+    return g
+
+
+def evaluate(a, b, landmarks):
+    """ Compare two particles based on distance, returning a prior probability.
+    """
+    noise = 0.1  # might want to start higher, then gradually anneal
+    prob = 1.0
+    for l in landmarks:
+        d1 = distance(a, l)
+        d2 = distance(b, l)
+        prob *= gaussian(d1, noise, d2)
+    return prob
+
+
+def move(init, m):
+    return (init[0] + m[0], init[1] + m[1])
+
+
+def converged(particles, tolerance):
+    """ Returns true when converged, based on tolerance """
+    if len(particles) <= 1:
+        return True
+    convg = all([distance(p[0], particles[0][0]) <= tolerance for p in particles])
+    return convg
+
+
+def example():
+    """ Example use of particle filter
+    """
+    mv = [(0, 0)] * 7
+    start = (12, 32)
+    landmarks = [(13, 14), (11, 46), (47, 4), (34, 17), (26, 25), (23, 55), (10, 10)]
+
+    d = 0.0
+    W = 100
+    particles = [((1, 1), 0), ((100, 100), 0)]
+    x = 0
+    while not (converged(particles, 0.001) or x > W):
+        best, position, particles = particle_filter(start, mv, evaluate, 1000, landmarks)
+        d += distance(best, position)
+        x += 1
+
+    print d / W
+    print position
+    print best
+    print x
--- readme.md
+++ readme.md
@@ -8,7 +8,11 @@ Reference or examples/templates for other projects.
 
 ### Kalman Filter
 
-A [Kalman filter](https://en.wikipedia.org/wiki/Kalman_filter) attempts to determine state from noisy input data, typically paired with positional shifting. In this implementation, the predict step offers a method of convolution (state becomes less certain) typical of movement and measurement (in which the state is likely to become more certain). Most common [applications](https://en.wikipedia.org/wiki/Kalman_filter#Applications) are in reference, navigation, and guidance.  
+A [Kalman filter](https://en.wikipedia.org/wiki/Kalman_filter) attempts to determine state from noisy input data, typically paired with positional shifting. In this implementation, the predict step offers a method of convolution (state becomes less certain) typical of movement and measurement (in which the state is likely to become more certain). Most common [applications](https://en.wikipedia.org/wiki/Kalman_filter#Applications) are in reference, navigation, and guidance.
+
+### Particle Filter
+
+A [particle filter](https://en.wikipedia.org/wiki/Particle_filter) also does state estimation, but has several advantages over Kalman filters and other methods. A pfilter can be used in continuous space and on multimodal distributions (Kalman filters only work on unimodal distributions). Implementation is also fairly easy. However, efficiency can suffer significantly in large state spaces. [Here](http://www.youtube.com/watch?v=4S-sx5_cmLU&feature=youtu.be&t=1m24s) is a good visualization of the process.
 
 ##  Credit
 
